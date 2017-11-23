87558ec4b7b09f056ca34828cbb728f29d2108e6
yuki3738 <y.minamiya@gmail.com>
2017-11-23 18:32:55 +0900

    3.3.2

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -10,20 +10,6 @@ def fizz_buzz(n)
   end
 end
 
-require 'minitest/autorun'
-
-class  FizzBuzzTest < Minitest::Test
-  def test_fizz_buzz
-    assert_equal '1', fizz_buzz(1)
-    assert_equal '2', fizz_buzz(2)
-    assert_equal 'Fizz', fizz_buzz(3)
-    assert_equal '4', fizz_buzz(4)
-    assert_equal 'Buzz', fizz_buzz(5)
-    assert_equal 'Fizz', fizz_buzz(6)
-    assert_equal 'Fizz Buzz', fizz_buzz(15)
-  end
-end
-
 # puts fizz_buzz(1)
 # puts fizz_buzz(2)
 # puts fizz_buzz(3)
---
+++ test/fizz_buzz_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require './lib/fizz_buzz'
+
+class  FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal '4', fizz_buzz(4)
+    assert_equal 'Buzz', fizz_buzz(5)
+    assert_equal 'Fizz', fizz_buzz(6)
+    assert_equal 'Fizz Buzz', fizz_buzz(15)
+  end
+end
+
+# puts fizz_buzz(1)
+# puts fizz_buzz(2)
+# puts fizz_buzz(3)
+# puts fizz_buzz(4)
+# puts fizz_buzz(5)
+# puts fizz_buzz(6)
+# puts fizz_buzz(15)
