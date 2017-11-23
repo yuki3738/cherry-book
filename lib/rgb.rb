979e1dd143e0a5b908268e329c6f83945a9dad5b
yuki3738 <y.minamiya@gmail.com>
2017-11-23 23:43:13 +0900

    4.6.3

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -3,3 +3,14 @@ def to_hex(r, g, b)
     hex + n.to_s(16).rjust(2, '0')
   end
 end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints = []
+  [r, g, b].each do |s|
+    ints << s.hex
+  end
+  ints
+end
