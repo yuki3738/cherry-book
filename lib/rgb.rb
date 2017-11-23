28a1252d49ddc0119dde7a6931b237e3ee56bdbd
yuki3738 <y.minamiya@gmail.com>
2017-11-23 21:56:13 +0900

    4.6.2

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,6 +1,5 @@
 def to_hex(r, g, b)
-  '#' +
-    r.to_s(16).rjust(2, '0') +
-    g.to_s(16).rjust(2, '0') +
-    b.to_s(16).rjust(2, '0')
+  [r, g, b].inject('#') do |hex, n|
+    hex + n.to_s(16).rjust(2, '0')
+  end
 end
