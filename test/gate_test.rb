6e3c5a58c8149514c158b386dcc28ed80fc3b0a2
yuki3738 <y.minamiya@gmail.com>
2017-12-01 20:09:50 +0900

    7.4.3

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -10,4 +10,13 @@ class GateTest < Minitest::Test
     umeda.enter(ticket)
     assert juso.exit(ticket)
   end
+
+  def test_umeda_to_mikuni_when_fare_is_not_enough
+    umeda = Gate.new(:umeda)
+    mikuni = Gate.new(:mikuni)
+
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    refute mikuni.exit(ticket)
+  end
 end
