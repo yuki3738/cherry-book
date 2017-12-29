e15619aea68a008a2a9a88c953a0acb61c269c03
yuki3738 <y.minamiya@gmail.com>
2017-12-29 18:32:18 +0900

    10.4.5

--- lib/effects.rb
+++ lib/effects.rb
@@ -4,4 +4,10 @@ module Effects
       words.split(' ').map(&:reverse).join(' ')
     end
   end
+
+  def self.echo(rate)
+    ->(words) do
+      words.chars.map { |c| c == ' ' ? c : c * rate }.join
+    end
+  end
 end
--- test/effects_test.rb
+++ test/effects_test.rb
@@ -6,4 +6,12 @@ class EffectsTest < Minitest::Test
     effect = Effects.reverse
     assert_equal 'ybuR si !nuf', effect.call('Ruby is fun!')
   end
+
+  def test_echo
+    effect = Effects.echo(2)
+    assert_equal 'RRuubbyy iiss ffuunn!!', effect.call('Ruby is fun!')
+
+    effect = Effects.echo(3)
+    assert_equal 'RRRuuubbbyyy iiisss fffuuunnn!!!', effect.call('Ruby is fun!')
+  end
 end
