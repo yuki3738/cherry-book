77e304eccc05416f773c36de239e9c826c9af8ef
yuki3738 <y.minamiya@gmail.com>
2017-12-29 18:21:22 +0900

    10.4.4

--- lib/effects.rb
+++ lib/effects.rb
@@ -1,4 +1,7 @@
 module Effects
   def self.reverse
+    ->(words) do
+      words.split(' ').map(&:reverse).join(' ')
+    end
   end
 end
--- test/effects_test.rb
+++ test/effects_test.rb
@@ -3,6 +3,7 @@ require './lib/effects'
 
 class EffectsTest < Minitest::Test
   def test_reverse
-    assert Effects
+    effect = Effects.reverse
+    assert_equal 'ybuR si !nuf', effect.call('Ruby is fun!')
   end
 end
